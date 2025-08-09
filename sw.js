2ebe4fb2151d40f1a67fa7bad7137754fc501436
v4ampira <amourbirlem@gmail.com>
2025-08-09 09:25:01 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-const CACHE = 'grimoire-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(['./','./index.html','./manifest.webmanifest'])));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
